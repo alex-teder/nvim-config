7b7ce5ab367986a3ce74b2b6a199371d06c3ef05
Aleksandr Teder <tederkedr@gmail.com>
2024-02-17 16:55:52 +0400

    added golang and python lsp stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,6 +10,9 @@ require('mason-lspconfig').setup({
   ensure_installed = {
     "tsserver",
     "eslint",
+    "pyright",
+    "gopls",
+    "golangci_lint_ls",
   },
   handlers = {
     lsp_zero.default_setup,
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "bash", "css", "scss", "html", "javascript", "jsdoc", "json", "json5", "typescript", "vue" },
+  ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "query", "bash", "css", "scss", "html", "javascript", "jsdoc", "json", "json5", "typescript", "vue", "go" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
