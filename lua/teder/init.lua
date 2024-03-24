09ac4d22e6f6f2d38bcc16519625415412ba0e4a
Aleksandr Teder <tederkedr@gmail.com>
2024-03-24 16:30:58 +0400

    added keymaps and descriptions

--- lua/teder/init.lua
+++ lua/teder/init.lua
@@ -6,6 +6,13 @@ require("teder.lazynvim")
 -- theme
 vim.cmd.colorscheme "catppuccin"
 
+-- highlight yanked text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- sets
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -20,11 +27,12 @@ vim.opt.updatetime = 50
 -- remaps
 vim.g.mapleader = " "
 
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = 'Switch to Normal mode' })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'move highlighted lines down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'move highlighted lines up' })
 
+-- when scrolling keep cursor in the center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
@@ -34,22 +42,26 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 
 local builtin = require('telescope.builtin')
 --vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Find file' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Grep in project' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fr', function() builtin.lsp_references({ show_line = false }) end,
+{ noremap = true, silent = true, desc = 'Find references' })
 
--- toggle file tree
-vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true })
+vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true, desc = 'Toggle file tree' })
 
--- show lsp error 
 vim.keymap.set(
     "n", 
     "<leader><space>", 
     ":lua vim.diagnostic.open_float()<CR>", 
-    { silent = true }
+    { silent = true, desc = 'Show diagnostics message' }
 )
 
-vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<CR>", { silent = true, desc = 'Next Hunk' })
+vim.keymap.set("n", "<leader>gN", ":Gitsigns prev_hunk<CR>", { silent = true, desc = 'Previous Hunk' })
+vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { silent = true, desc = 'Preview Hunk' })
+vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { silent = true, desc = 'Reset Hunk' })
+vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_buffer<CR>", { silent = true, desc = 'Reset Buffer' })
 
-vim.keymap.set("n", "<leader>vs", ":vsplit<CR>", {})
+vim.keymap.set("n", "<leader>vs", ":vsplit<CR>", { desc = 'Vertical split' })
